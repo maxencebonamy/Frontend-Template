ca66011bc9f44af04de5a295d1d1d717cd9bb734
Maxence Bonamy <maxencebonamy@gmail.com>
2023-12-21 16:49:38 +0100

    Change location of CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
 import { Poppins } from "next/font/google"
-import "./globals.css"
+import "@/lib/styles/globals.css"
 import type { PropsWithChildren, ReactElement } from "react"
 
 const font = Poppins({
